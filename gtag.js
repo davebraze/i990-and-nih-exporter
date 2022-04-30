720664f6f61c2ff3ce7c8b08a3041baa5a7be8da
Dave Braze <davebraze@gmail.com>
2022-04-30 14:51:09 -0400

    move toward web page release

---
+++ gtag.js
@@ -0,0 +1,11 @@
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-532395-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-532395-3');
+</script>
+
